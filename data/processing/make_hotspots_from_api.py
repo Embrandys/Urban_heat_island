78f63278f47ff611e3db351548c5e0bee40ebbac
Embrandys <embrandys236@gmail.com>
2026-02-08 18:22:58 +0100

    import pyproj

--- data/processing/make_hotspots_from_api.py
+++ data/processing/make_hotspots_from_api.py
@@ -15,12 +15,12 @@ import planetary_computer as pc
 
 
 # --- PROJ fix ----------------
-venv = os.environ.get("VIRTUAL_ENV")
-if venv:
-    proj_data = Path(venv) / "Lib" / "site-packages" / "rasterio" / "proj_data"
-    if proj_data.exists():
-        os.environ["PROJ_LIB"] = str(proj_data)
-        os.environ.pop("PROJ_DATA", None)
+# venv = os.environ.get("VIRTUAL_ENV")
+# if venv:
+#     proj_data = Path(venv) / "Lib" / "site-packages" / "rasterio" / "proj_data"
+#     if proj_data.exists():
+#         os.environ["PROJ_LIB"] = str(proj_data)
+#         os.environ.pop("PROJ_DATA", None)
 
 
 # --------- konfiguracja ---------
